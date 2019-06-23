09e7065cb45b26bf839f6ee877b63f6cd758d98f
Alex Vassiliou <alex@20diamond.com>
2019-06-23 17:16:29 +1000

    app rendering on page

---
+++ src/components/search_bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return() {
+      <input type="text" className="form-control form-search"/>
+    }
+  }
+}
+
+export default SearchBar;
